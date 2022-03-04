20da82b991a6cb7936e0b8ca65b85ed6b70d00ae
jay28122212 <95669684+jay28122212@users.noreply.github.com>
2022-03-04 17:29:54 +0530

    Add files via upload

---
+++ models/order.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose')
+const ObjectID = mongoose.Schema.Types.ObjectId
+
+
+const orderSchema = new mongoose.Schema({
+    owner : {
+      type: ObjectID,
+       required: true,
+       ref: 'User'
+     },
+    items: [{
+      itemId: {
+       type: ObjectID,
+       ref: 'Item',
+       required: true
+    },
+    name: String,
+    quantity: {
+       type: Number,
+       required: true,
+       min: 1,
+       default: 1},             
+
+       price: Number
+       
+     }],
+    bill: {
+        type: Number,
+        required: true,
+       default: 0
+      },
+      address: { type: Object, required: true },
+      status: { type: String, default: "pending" },
+    }, {
+
+    timestamps: true
+
+    })
+  const Order = mongoose.model('Order', orderSchema)
+  
+module.exports = Order
+
+
+
